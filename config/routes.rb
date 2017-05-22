f13ac7f2b0c6216cba938008d1bdcc14e1ef16a3
Phillip Medrano <phillip.medrano@gmail.com>
2017-05-21 23:45:32 -0700

    removed unused views and routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,26 +7,6 @@ Spree::Core::Engine.routes.draw do
         post :sample_csv_import
         post :user_csv_import
         get :download_sample_csv
-        post :shopify_csv_import
-        get :download_sample_shopify_export_csv
-      end
-    end
-    resources :order_imports, only: [:index] do
-      collection do
-        get :reset
-        get :sample_import
-        post :sample_csv_import
-        post :user_csv_import
-        get :download_sample_csv
-      end
-    end
-    resources :user_imports, only: [:index] do
-      collection do
-        get :reset
-        get :sample_import
-        post :sample_csv_import
-        post :user_csv_import
-        get :download_sample_csv
       end
     end
   end
