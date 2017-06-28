cecf7e31f6bf10f64c1ae4af5c47512937187bd2
Phillip Medrano <phillip.medrano@gmail.com>
2017-06-28 00:14:28 -0700

    clear product data not available in production

--- app/overrides/add_import_products_to_products_sub_menu.rb
+++ app/overrides/add_import_products_to_products_sub_menu.rb
@@ -2,6 +2,6 @@ Deface::Override.new(
   virtual_path: 'spree/admin/shared/sub_menu/_product',
   name: 'import_products_tab',
   insert_bottom: "#sidebar-product",
-  text: %Q{ <%= tab :product_imports %> },
+  text: %Q{ <%= tab :product_imports, label: 'Imports / Exports' %> },
   original: '41d2419e728f18bfaf1ccf2ca0935f2c321bdf0c'
 )
